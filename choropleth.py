b6dd4b148b61fb80da21efc7d7010795cf6f857b
StochajK <stochaj.k@northeastern.edu>
2022-04-07 10:49:03 -0400

    Making the energy data easier to read/work with

--- choropleth.py
+++ choropleth.py
@@ -5,7 +5,7 @@ Code for Final Project - Choropleth Map
 7 April, 2022
 choropleth.py
 """
-# Import necessary libraries and packages\
+# Import necessary libraries and packages
 import folium
 import pandas as pd
 import json
@@ -13,8 +13,7 @@ from folium import plugins
 
 # Define necessary constants for files
 MAP = "countries.geojson"
-ENERGY = "GlobalElectricityGeneration.csv"
-
+ENERGY = "2019_global_energy_data.csv"
 
 def main():
     
