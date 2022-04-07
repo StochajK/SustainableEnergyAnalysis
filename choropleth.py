5b2e3799ee1e3eb1480e14efe9dd33a9842923da
StochajK <stochaj.k@northeastern.edu>
2022-04-07 12:37:54 -0400

    in class push

--- choropleth.py
+++ choropleth.py
@@ -10,12 +10,19 @@ import folium
 import pandas as pd
 import json
 from folium import plugins
+import plotly.express as px
 
 # Define necessary constants for files
 MAP = "countries.geojson"
 ENERGY = "2019_global_energy_data.csv"
 
 def main():
+    """
+    data = pd.read_json(MAP)
+    print(data["features"][1]["properties"])
+    """
+    df = pd.read_csv(ENERGY)
+    print(df)
     
     
     
