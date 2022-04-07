11d863ddb5f695696f704543b9059afdc18d86fc
StochajK <stochaj.k@northeastern.edu>
2022-04-07 10:31:53 -0400

    Initial commit
    Added barebones python file and all necessary data files for the construction of our choropleth map

---
+++ choropleth.py
@@ -0,0 +1,23 @@
+"""
+Kimberly Stochaj
+DS2000
+Code for Final Project - Choropleth Map
+7 April, 2022
+choropleth.py
+"""
+# Import necessary libraries and packages\
+import folium
+import pandas as pd
+import json
+from folium import plugins
+
+# Define necessary constants for files
+MAP = "countries.geojson"
+ENERGY = "GlobalElectricityGeneration.csv"
+
+
+def main():
+    
+    
+    
+main()
