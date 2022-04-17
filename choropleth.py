e820de7745179b7af433f754021f666ab206726b
StochajK <stochaj.k@northeastern.edu>
2022-04-17 12:08:36 -0400

    Progress 4/16

--- choropleth.py
+++ choropleth.py
@@ -6,24 +6,47 @@ Code for Final Project - Choropleth Map
 choropleth.py
 """
 # Import necessary libraries and packages
-import folium
-import pandas as pd
 import json
-from folium import plugins
-import plotly.express as px
+# import plotly.express as px
+# import pandas as pd
+from energy_stat import EnergyStat
 
 # Define necessary constants for files
-MAP = "countries.geojson"
-ENERGY = "2019_global_energy_data.csv"
+ENERGY_JSON = "EnergyDataNew.json"
 
 def main():
-    """
-    data = pd.read_json(MAP)
-    print(data["features"][1]["properties"])
-    """
-    df = pd.read_csv(ENERGY)
-    print(df)
+    # Read in the JSON file
+    with open(ENERGY_JSON, "r") as infile:
+        data = json.load(infile)
     
+    # Construct empty lists for storing the energy objects
+    renewable_energy_data = []
+    total_energy_data = []
+    
+    
+    for item in data:
+        if type(item["data"][0]["value"]) == float:
+            energy_obj = EnergyStat(item["name"], item["data"][0]["value"], item["iso"])
+        else:
+            energy_obj = EnergyStat(item["name"], "Nan", item["iso"])
+        
+        if energy_obj.source == "Renewable electricity net generation":
+            renewable_energy_data.append(energy_obj)
+        elif energy_obj.source == "Electricity net generation":
+            total_energy_data.append(energy_obj)
+            
+    for country in total_energy_data:
+        print(country)
+        
+        
+    '''
+    print(data[0]["name"])
+    energy_example = EnergyStat(data[0]["name"], data[0]["data"][0]["value"], data[0]["iso"])
+    print("Source =", energy_example.source)
+    print("Country =", energy_example.country_name)
+    print("Amount =", energy_example.amt)
+    print("Code =", energy_example.country_code)
+    '''
     
     
 main()
---
+++ energy_stat.py
@@ -0,0 +1,31 @@
+"""
+Kimberly Stochaj
+DS2001
+Practicum project - creating a class to hold data used in cloropleth.py
+16 April, 2022
+energy_stat.py
+"""
+
+class EnergyStat:
+    """ The EnergyStat class is a clean way of holding the important
+        information retrieved from the JSON file that is needed to make the
+        cloropleth map
+    """
+    def __init__(self, name_string, amt, code):
+        """ constructor for an EnergyStat object
+        """
+        self.amt = amt
+        self.country_name, self.source = self.get_info(name_string)
+        self.country_code = code
+    
+    def get_info(self, info_str):
+        """ returns the country and the energy source from the provided string
+            from the JSON file... string format is "source, country, period"
+        """
+        lst = info_str.split(",")
+        return (lst[1], lst[0])
+    
+    def __str__(self):
+        return self.country_name + " produces " + str(self.amt) + " for " + self.source 
+        
+        
